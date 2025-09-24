babbc3c14211193187e1cf7328f6c21224be7d2b
I'm_Cryinggg <daniel.paras.mail@gmail.com>
2025-09-24 18:00:16 +0800

    Revert "lot-calculator-v1.0.2"
    This reverts commit 1abc2b1128a2ef54417b4f3eedbde5ba9fd7b737.

--- ConsoleApp1-activity/Program.cs
+++ ConsoleApp1-activity/Program.cs
@@ -1,13 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // version 1.0.0
 // this is for commit and push branch (updated nasdaq calculation)
-// this for pull branch (added a new pair for forex)
 
 while (true)
 {
     Console.WriteLine("List of pairs:");
-    Console.WriteLine("NASDAQ (indices)\n");
-    Console.WriteLine("AUDCAD (forex)\n");
+    Console.WriteLine("NASDAQ - Indices\n");
     Console.WriteLine("Please select what pair to trade:");
     string pair = Console.ReadLine();
     Console.WriteLine();
@@ -25,48 +23,23 @@ while (true)
     double pips = Convert.ToDouble(Console.ReadLine());
     Console.WriteLine();
 
-    double position = 0;
-    bool validpair = true;
-
-    switch (pair.ToUpper())
+    switch (pair)
     {
-        case "NASDAQ":
-            position = nasdaq(risk_amount, pips);
+        case "NASDAQ" or "nasdaq":
+            double position = nasdaq(risk_amount, pips);
+            Console.Write($"POSITION: {position}");
+            Console.ReadKey();
             break;
-        case "AUDCAD":
-            position = audcad(risk_amount, pips);
-            break;
-        default:
-            Console.WriteLine("Invalid pair selection. Please choose from the options");
-            validpair = false;
-            break;
-    }
-    if (validpair)
-    {
-        Console.WriteLine($"POSITION: {position}");
-        Console.WriteLine("Press any key to continue...");
-        Console.ReadKey();
     }
     Console.WriteLine();
 }
+Console.ReadKey();
+
 static double nasdaq(double risk_amount, double pips)
 {
-    double contract_size = 10; // Standard lot size for indices
+    double contract_size = 10;
     double new_pips = pips * 100;
-    double position = risk_amount / new_pips * contract_size;
+    double position = (risk_amount / new_pips) * contract_size;
     double lot_size = Math.Round(position, 2);
     return lot_size;
 }
-
-static double audcad(double risk_amount, double pips)
-{
-    double contract_size = 7.25; // Standard lot size for indices
-    double new_pips = pips;
-    double position = risk_amount / new_pips * contract_size;
-    double lot_size = Math.Round(position, 2);
-    return lot_size;
-}
-
-
-
-
